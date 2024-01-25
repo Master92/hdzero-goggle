8fd3d37ea93011a1cd8f05ae80fe2cb1da7d0348
Master92 <nils.friedchen@googlemail.com>
2024-01-25 13:08:22 +0100

    Change type of media database to be a media node

--- src/ui/page_playback.h
+++ src/ui/page_playback.h
@@ -25,19 +25,14 @@ extern "C" {
 
 #define MAX_VIDEO_FILES 999
 
-typedef struct {
+typedef struct media_file_node {
     char filename[64];
     char label[64];
     char ext[16];
     int size;
     bool star;
+    struct media_file_node* children;
 } media_file_node_t;
-
-typedef struct {
-    media_file_node_t list[MAX_VIDEO_FILES];
-    int count;
-    int cur_sel;
-} media_db_t;
 
 typedef struct {
     lv_obj_t *_img;
